d107c9b1f302bd25645214137992607f02b532ab
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-09 11:05:23 +0600

    fetching products data

---
+++ src/components/Page.jsx
@@ -0,0 +1,10 @@
+import ProductList from "./ProductList";
+
+export default function Page() {
+    return (
+        <>
+            <h1 className="text-3xl bg-yellow-400 font-bold text-center" >Kishor Kumar Paroi</h1>
+            <ProductList />
+        </>
+    )
+}
