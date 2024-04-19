b17fafafa93dd8bebe59778959c3f8626d9d510e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-19 20:31:12 +0600

    created skeleton pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import NotFoundPage from './components/NotFoundPage';
+import ProfilePage from './components/ProfilePage';
+import RegistrationPage from './components/RegistrationPage';
+
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl bg-yellow-500 text-center ">Kishor Kumar Paroi</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="/me" element={<ProfilePage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+
+export default function ProfilePage() {
+    return (
+        <div>
+            <h1 className="text-3xl bg-yellow-500 text-center ">Kishor Kumar Paroi</h1>
+        </div>
+    )
+}
