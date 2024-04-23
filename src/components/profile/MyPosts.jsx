9961d4c9cc2ccc88c5080c5ecf77ac543e6dad55
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-23 11:45:25 +0600

    fetching post comments, actions and posts

--- src/components/profile/MyPosts.jsx
+++ src/components/profile/MyPosts.jsx
@@ -1,6 +1,15 @@
+import { useProfile } from "../../hooks";
+import PostList from '../posts/PostList';
+
 
 export default function MyPosts() {
+    const { state } = useProfile();
+    const posts = state?.posts;
+
     return (
-        <div>MyPosts</div>
+        <>
+            <h4 className="mt-6 text-2xl lg:text-3xl lg:mt-8 font-bold">Your Posts</h4>
+            <PostList posts={posts} />
+        </>
     )
 }
