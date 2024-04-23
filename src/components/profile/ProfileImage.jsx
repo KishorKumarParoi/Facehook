6f13c12acf31aa4e8da65a8dee32b9f0fd74ce48
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-23 17:42:20 +0600

    Registration feature and default avatar

--- src/components/profile/ProfileImage.jsx
+++ src/components/profile/ProfileImage.jsx
@@ -7,7 +7,9 @@ import { useProfile } from "../../hooks";
 
 export default function ProfileImage() {
     const { state, dispatch } = useProfile();
+    console.log("🚀 ~ ProfileImage ~ state:", state)
     const fileUploaderRef = useRef();
+
 
     const updateImageDisplay = async () => {
         try {
@@ -34,7 +36,7 @@ export default function ProfileImage() {
 
     const handleImageUpload = (event) => {
         event.preventDefault();
-        fileUploaderRef.current.addEventLister('change', updateImageDisplay);
+        fileUploaderRef.current.addEventListener('change', updateImageDisplay);
         fileUploaderRef.current.click();
     }
 
@@ -42,8 +44,7 @@ export default function ProfileImage() {
         <div className="relative mb-8 max-h-[180px] max-w-[180px] rounded-full lg:mb-11 lg:max-h-[218px] lg:max-w-[218px] ">
             <img
                 className="max-w-full rounded-full"
-                src={`${import.meta.env.VITE_SERVER_BASE_URL}/${state?.user?.avatar
-                    }`}
+                src={`${import.meta.env.VITE_SERVER_BASE_URL}/${state?.user?.avatar}`}
                 alt={state?.user?.firstName}
             />
 
