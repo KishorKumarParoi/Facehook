4d380e484be0eca7d6f8fef3feabf130bdd61100
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-24 14:54:03 +0600

    Posting comment real time

--- src/components/posts/PostComments.jsx
+++ src/components/posts/PostComments.jsx
@@ -1,20 +1,42 @@
 import { useState } from "react";
-import { useAvatar } from "../../hooks/useAvatar";
+import { useAuth } from "../../hooks";
+import useAxios from "../../hooks/useAxios";
 import PostCommentList from "./PostCommentList";
 
 const PostComments = ({ post }) => {
-    const { avatarURL } = useAvatar(post);
-    const [showAction, setShowAction] = useState(true);
+    const { auth } = useAuth();
+    const [comments, setComments] = useState(post?.comments);
+    const [comment, setComment] = useState("");
+    const { api } = useAxios();
 
-    function toggleAction() {
-        setShowAction(!showAction);
-    }
+    const addComment = async (event) => {
+        const keyCode = event.keyCode;
+
+        if (keyCode === 13) {
+            console.log("Adding comment");
+            try {
+                const response = await api.patch(
+                    `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id
+                    }/comment`,
+                    { comment }
+                );
+
+                if (response.status === 200) {
+                    setComments([...response.data.comments]);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
     return (
         <div>
             <div className="flex-center mb-3 gap-2 lg:gap-4">
                 <img
                     className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-                    src={avatarURL}
+                    src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar
+                        }`}
                     alt="avatar"
                 />
 
@@ -24,17 +46,20 @@ const PostComments = ({ post }) => {
                         className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
                         name="post"
                         id="post"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        onKeyDown={(e) => addComment(e)}
                         placeholder="What's on your mind?"
                     />
                 </div>
             </div>
             <div className="mt-4">
-                <button className="text-gray-300 max-md:text-sm" onClick={toggleAction}>
+                <button className="text-gray-300 max-md:text-sm">
                     All Comment ▾
                 </button>
             </div>
 
-            {showAction && <PostCommentList comments={post?.comments} />}
+            <PostCommentList comments={comments} />
         </div>
     );
 };
